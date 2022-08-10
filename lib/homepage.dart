806cef7e22b157dc5d09152048f770a17dd214cd
nosgnille05 <telli035@fiu.edu>
2022-08-09 23:09:34 -0700

    Added Jump Functionality (Gravity)

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flappy_bird_clone/bird.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  static double birdYaxis = 0;
+  double time = 0, height = 0, initialHeight = birdYaxis, velocity = 2.8;
+  bool gameStarted = false;
+
+  void jump() {
+    setState(() {
+      time = 0;
+      initialHeight = birdYaxis;
+    });
+  }
+
+  void startGame() {
+    gameStarted = true;
+    Timer.periodic(Duration(milliseconds: 50), (timer) {
+      time += 0.04;
+      height = -4.9 * time * time + velocity * time;
+      setState(() {
+        birdYaxis = initialHeight - height;
+      });
+      if (birdYaxis > 1) {
+        timer.cancel();
+        gameStarted = false;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +41,20 @@ class _HomePageState extends State<HomePage> {
         children: (<Widget>[
           Expanded(
             flex: 3,
-            child: Container(
-              color: Colors.blue,
-              child: Center(child: MyBird()),
+            child: GestureDetector(
+              onTap: () {
+                if (gameStarted) {
+                  jump();
+                } else {
+                  startGame();
+                }
+              },
+              child: AnimatedContainer(
+                alignment: Alignment(0, birdYaxis),
+                duration: Duration(milliseconds: 0),
+                color: Colors.blue,
+                child: MyBird(),
+              ),
             ),
           ),
           Expanded(
