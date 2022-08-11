2d53b5f2df69d65362fa344886434d05b111221a
nosgnille05 <telli035@fiu.edu>
2022-08-10 21:02:19 -0700

    Game Logic, Play Again

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,4 @@
-//ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+//ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, deprecated_member_use, prefer_interpolation_to_compose_strings
 
 import 'dart:async';
 import 'package:flappy_bird_clone/barriers.dart';
@@ -14,8 +14,9 @@ class _HomePageState extends State<HomePage> {
   static double birdYaxis = 0;
   double time = 0, height = 0, initialHeight = birdYaxis, velocity = 2.8;
   bool gameStarted = false;
-  static double barrierXone = 1.5;
-  double barrierXtwo = barrierXone + 1.35;
+  static double barrierXone = 1.7;
+  static double barrierXtwo = barrierXone + 1.7;
+  //double barrierXthree = barrierXtwo + 1.7;
 
   void jump() {
     setState(() {
@@ -31,25 +32,31 @@ class _HomePageState extends State<HomePage> {
       height = -4.9 * time * time + velocity * time;
       setState(() {
         birdYaxis = initialHeight - height;
-        barrierXone -= 0.05;
-        barrierXtwo -= 0.05;
       });
 
       setState(() {
-        if (barrierXone < -1.1) {
-          barrierXone += 2.2;
+        if (barrierXone < -1.7) {
+          barrierXone += 3.4;
         } else {
-          barrierXone -= 0.5;
+          barrierXone -= 0.05;
         }
       });
 
       setState(() {
-        if (barrierXtwo < -1.1) {
-          barrierXone += 2.2;
+        if (barrierXtwo < -1.7) {
+          barrierXtwo += 3.4;
         } else {
-          barrierXtwo -= 0.5;
+          barrierXtwo -= 0.05;
         }
       });
+
+      /*setState(() {
+        if (barrierXthree < -1.7) {
+          barrierXthree += 3.4;
+        } else {
+          barrierXthree -= 0.05;
+        }
+      });*/
 
       if (birdYaxis > 1) {
         timer.cancel();
@@ -58,109 +65,164 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  int score = 101;
+  int highscore = 101;
+
+  void _ShowDialog() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.brown,
+            title: Text(
+              "GAME OVER",
+              style: TextStyle(color: Colors.white),
+            ),
+            content: Text(
+              "Score " + score.toString(),
+              style: TextStyle(color: Colors.white),
+            ),
+            actions: [
+              FlatButton(
+                  child: Text(
+                    "PLAY AGAIN",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    if (score > highscore) {
+                      highscore = score;
+                    }
+                    initState();
+                    setState(() {
+                      gameStarted = false;
+                    });
+                    Navigator.of(context).pop();
+                  })
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: (<Widget>[
-          Expanded(
-            flex: 3,
-            child: Stack(
-              children: [
-                GestureDetector(
-                  onTap: () {
-                    if (gameStarted) {
-                      jump();
-                    } else {
-                      startGame();
-                    }
-                  },
-                  child: AnimatedContainer(
+    return GestureDetector(
+      onTap: () {
+        if (gameStarted) {
+          jump();
+        } else {
+          startGame();
+        }
+      },
+      child: Scaffold(
+        body: Column(
+          children: (<Widget>[
+            Expanded(
+              flex: 3,
+              child: Stack(
+                children: [
+                  AnimatedContainer(
                     alignment: Alignment(0, birdYaxis),
                     duration: Duration(milliseconds: 0),
                     color: Colors.blue,
                     child: MyBird(),
                   ),
-                ),
-                Container(
-                    alignment: Alignment(0, -0.3),
-                    child: gameStarted
-                        ? Text(" ")
-                        : Text("T A P   T O   P L A Y",
-                            style: TextStyle(
-                              fontSize: 20,
-                              color: Colors.white,
-                            ))),
-                AnimatedContainer(
-                  alignment: Alignment(barrierXone, 1.1),
-                  duration: Duration(milliseconds: 0),
-                  child: MyBarrier(
-                    size: 200.0,
+                  Container(
+                      alignment: Alignment(0, -0.3),
+                      child: gameStarted
+                          ? Text(" ")
+                          : Text("T A P   T O   P L A Y",
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.white,
+                              ))),
+                  AnimatedContainer(
+                    alignment: Alignment(barrierXone, 1.1),
+                    duration: Duration(milliseconds: 0),
+                    child: MyBarrier(
+                      size: 200.0,
+                    ),
                   ),
-                ),
-                AnimatedContainer(
-                  alignment: Alignment(barrierXone, -1.1),
-                  duration: Duration(milliseconds: 0),
-                  child: MyBarrier(
-                    size: 200.0,
+                  AnimatedContainer(
+                    alignment: Alignment(barrierXone, -1.1),
+                    duration: Duration(milliseconds: 0),
+                    child: MyBarrier(
+                      size: 200.0,
+                    ),
                   ),
-                ),
-                AnimatedContainer(
-                  alignment: Alignment(barrierXtwo, 1.1),
-                  duration: Duration(milliseconds: 0),
-                  child: MyBarrier(
-                    size: 150.0,
+                  AnimatedContainer(
+                    alignment: Alignment(barrierXtwo, 1.1),
+                    duration: Duration(milliseconds: 0),
+                    child: MyBarrier(
+                      size: 150.0,
+                    ),
                   ),
-                ),
-                AnimatedContainer(
-                  alignment: Alignment(barrierXtwo, -1.1),
-                  duration: Duration(milliseconds: 0),
-                  child: MyBarrier(
-                    size: 250.0,
+                  AnimatedContainer(
+                    alignment: Alignment(barrierXtwo, -1.1),
+                    duration: Duration(milliseconds: 0),
+                    child: MyBarrier(
+                      size: 250.0,
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            height: 15,
-            color: Colors.green,
-          ),
-          Expanded(
-            child: Container(
-              color: Colors.brown,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("SCORE",
-                          style: TextStyle(color: Colors.white, fontSize: 20)),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text("0",
-                          style: TextStyle(color: Colors.white, fontSize: 35))
-                    ],
+                  /*AnimatedContainer(
+                    alignment: Alignment(barrierXthree, 1.1),
+                    duration: Duration(milliseconds: 0),
+                    child: MyBarrier(
+                      size: 300.0,
+                    ),
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("BEST",
-                          style: TextStyle(color: Colors.white, fontSize: 20)),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text("10",
-                          style: TextStyle(color: Colors.white, fontSize: 35)),
-                    ],
-                  ),
+                  AnimatedContainer(
+                    alignment: Alignment(barrierXthree, -1.1),
+                    duration: Duration(milliseconds: 0),
+                    child: MyBarrier(
+                      size: 100.0,
+                    ),
+                  ),*/
                 ],
               ),
             ),
-          ),
-        ]),
+            Container(
+              height: 15,
+              color: Colors.green,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.brown,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("SCORE",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20)),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text("0",
+                            style: TextStyle(color: Colors.white, fontSize: 35))
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("BEST",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 20)),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Text("10",
+                            style:
+                                TextStyle(color: Colors.white, fontSize: 35)),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ),
       ),
     );
   }
